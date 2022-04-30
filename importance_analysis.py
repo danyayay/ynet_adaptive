5e75c620c12f6160886cab9016f10f453f679ddc
Danya Li <danya_li@outlook.com>
2022-04-30 15:04:47 +0200

    two ways of importance analysis

--- importance_analysis.py
+++ importance_analysis.py
@@ -1,11 +1,7 @@
 import os
 import yaml
-import time
 import pathlib
 import argparse
-import numpy as np
-import pandas as pd
-
 from model import YNetTrainer
 from utils.dataset import set_random_seeds, dataset_split, dataset_given_scenes
 from utils.visualize import plot_importance_analysis
@@ -37,44 +33,174 @@ def main(args):
         _, df_test, _ = dataset_split(DATA_PATH, args.files, 0)
     print(f"df_test: {df_test.shape}; #={df_test.shape[0]/(params['obs_len']+params['pred_len'])}")
 
-    # ## model 
-    # pretrained model 
-    pretrained_model = YNetTrainer(params=params)
-    pretrained_model.load(args.pretrained)
-    _, _, list_metrics = pretrained_model.test(df_test, IMAGE_PATH, False, False, False) 
+    # dir 
     folder_name = f"{args.seed}__{args.pretrained.split('_filter_')[1].split('__')[0]}__{'_'.join(args.files).rstrip('.pkl')}" 
-    out_dir = f"csv/importance_analysis/{folder_name}"
-    pathlib.Path(out_dir).mkdir(parents=True, exist_ok=True)
-    list_metrics[0].to_csv(
-        f"{out_dir}/OODG__N{'_'.join(str(n) for n in args.n_leftouts)}.csv", index=False)
-    print('Saved pretrained predictions')
-
-    # tuned models 
-    for tuned, tuned_name in zip(args.tuned, args.tuned_name):
-        print(f'====== Testing for {tuned_name} ======')
+    if args.replace_tuned:
+        out_dir_tuned = f"csv/importance_analysis/{folder_name}/{args.depth}/replace_tuned"
+        pathlib.Path(out_dir_tuned).mkdir(parents=True, exist_ok=True)
+    if args.replace_pretrained:
+        out_dir_pretrained = f"csv/importance_analysis/{folder_name}/{args.depth}/replace_pretrained"
+        pathlib.Path(out_dir_pretrained).mkdir(parents=True, exist_ok=True)
+        
+    # ## model 
+    if args.generate_csv:
+        # pretrained model 
+        pretrained_model = YNetTrainer(params=params)
+        pretrained_model.load(args.pretrained)
         set_random_seeds(args.seed)
-        tuned_model = YNetTrainer(params=params)
-        tuned_model.load(tuned)
-        _, _, list_metrics = tuned_model.test(df_test, IMAGE_PATH, False, False, False) 
-        list_metrics[0].to_csv(
-            f"{out_dir}/{tuned_name}__N{'_'.join(str(n) for n in args.n_leftouts)}.csv", index=False)
-        # replace one/several layers in tuned model by pretrained model 
-        for param_name, param in pretrained_model.model.named_parameters():
-            if not param_name.startswith('semantic_segmentation'):
-                tuned_model.load(tuned)
-                print(f'Replacing {param_name}')
-                tuned_model.model.load_state_dict({param_name: param}, strict=False)
-                _, _, list_metrics = tuned_model.test(df_test, IMAGE_PATH, False, False, False) 
-                # store ade/fde 
-                out_path = f"{out_dir}/{tuned_name}__N{'_'.join(str(n) for n in args.n_leftouts)}__{param_name}.csv"
-                list_metrics[0].to_csv(out_path, index=False)
-    
+        _, _, list_metrics_pretrained = pretrained_model.test(df_test, IMAGE_PATH, False, False, False) 
+        if args.replace_tuned:
+            list_metrics_pretrained[0].to_csv(
+                f"{out_dir_tuned}/OODG__N{'_'.join(str(n) for n in args.n_leftouts)}.csv", index=False)
+        if args.replace_pretrained:
+            list_metrics_pretrained[0].to_csv(
+                f"{out_dir_pretrained}/OODG__N{'_'.join(str(n) for n in args.n_leftouts)}.csv", index=False)
+        print('Saved pretrained predictions')
 
-def main(args):
-    plot_importance_analysis(
-        'csv/importance_analysis/1__agent_type__Biker', 
-        'figures/importance_analysis/1__agent_type__Biker',
-        n_test=args.n_leftouts)
+        if args.depth == -1:
+            # tuned models 
+            for tuned, tuned_name in zip(args.tuned, args.tuned_name):
+                print(f'====== Testing for {tuned_name} ======')
+                tuned_model = YNetTrainer(params=params)
+                tuned_model.load(tuned)
+                set_random_seeds(args.seed)
+                _, _, list_metrics_tuned = tuned_model.test(df_test, IMAGE_PATH, False, False, False)         
+                
+                # replace one layers in tuned model by pretrained model 
+                if args.replace_tuned:
+                    list_metrics_tuned[0].to_csv(
+                        f"{out_dir_tuned}/{tuned_name}__N{'_'.join(str(n) for n in args.n_leftouts)}.csv", index=False)
+                    for param_name, param in pretrained_model.model.named_parameters():
+                        # if (tuned_name == 'ET' and param_name.startswith('encoder')) or \
+                        #     (tuned_name == 'FT' and not param_name.startswith('semantic_segmentation')):
+                        if not param_name.startswith('semantic_segmentation'):
+                            tuned_model.load(tuned)
+                            print(f'Replacing {param_name}')
+                            tuned_model.model.load_state_dict({param_name: param}, strict=False)
+                            # sanity check 
+                            # for i in tuned_model.model.state_dict().keys(): print(i, (tuned_model.model.state_dict()[i] == pretrained_model.model.state_dict()[i]).all().item())
+                            set_random_seeds(args.seed)
+                            _, _, list_metrics = tuned_model.test(df_test, IMAGE_PATH, False, False, False) 
+                            # store ade/fde 
+                            out_path = f"{out_dir_tuned}/{tuned_name}__N{'_'.join(str(n) for n in args.n_leftouts)}__{param_name}.csv"
+                            list_metrics[0].to_csv(out_path, index=False)
+                
+                # replace one layer in pretrained model by tuned model 
+                if args.replace_pretrained:
+                    list_metrics_tuned[0].to_csv(
+                        f"{out_dir_pretrained}/{tuned_name}__N{'_'.join(str(n) for n in args.n_leftouts)}.csv", index=False)
+                    tuned_model.load(tuned)
+                    for param_name, param in tuned_model.model.named_parameters():
+                        if not param_name.startswith('semantic_segmentation'):
+                            pretrained_model.load(args.pretrained)
+                            print(f'Replacing {param_name}')
+                            pretrained_model.model.load_state_dict({param_name: param}, strict=False)
+                            set_random_seeds(args.seed)
+                            _, _, list_metrics = pretrained_model.test(df_test, IMAGE_PATH, False, False, False) 
+                            # store ade/fde 
+                            out_path = f"{out_dir_pretrained}/{tuned_name}__N{'_'.join(str(n) for n in args.n_leftouts)}__{param_name}.csv"
+                            list_metrics[0].to_csv(out_path, index=False)
+        
+        elif (args.depth == 1) | (args.depth == 2):
+            # group parameters name 
+            state_dict = pretrained_model.model.state_dict().keys()
+            params_selected = []
+            # encoder
+            for param in state_dict:
+                if param.startswith('encoder'):
+                    if args.depth == 1:
+                        p_short = '.'.join(param.split('.')[:-2])
+                    elif args.depth == 2:
+                        p_short = '.'.join(param.split('.')[:-1])
+                    if p_short not in params_selected:
+                        params_selected.append(p_short)
+            # decoder
+            for decoder in ['goal_decoder', 'traj_decoder']:
+                if args.depth == 1:
+                    params_selected.append(f'{decoder}.center')
+                    for i in range(5):
+                        params_selected.append(
+                            [f'{decoder}.upsample_conv.{i}', f'{decoder}.decoder.{i}'])
+                    params_selected.append(f'{decoder}.predictor')
+                elif args.depth == 2:
+                    for param in state_dict:
+                        if param.startswith(decoder):
+                            p_short = '.'.join(param.split('.')[:-1])
+                            if p_short not in params_selected:
+                                params_selected.append(p_short)
+
+            # tuned models 
+            for tuned, tuned_name in zip(args.tuned, args.tuned_name):
+                print(f'====== Testing for {tuned_name} ======')
+                tuned_model = YNetTrainer(params=params)
+                tuned_model.load(tuned)
+                set_random_seeds(args.seed)
+                _, _, list_metrics_tuned = tuned_model.test(df_test, IMAGE_PATH, False, False, False)         
+                
+                if args.replace_tuned:
+                    list_metrics_tuned[0].to_csv(
+                        f"{out_dir_tuned}/{tuned_name}__N{'_'.join(str(n) for n in args.n_leftouts)}.csv", index=False)
+                    for param_selected in params_selected:
+                        tuned_model.load(tuned)
+                        for param_name, param in pretrained_model.model.named_parameters():
+                            if not param_name.startswith('semantic_segmentation'):
+                                # "semantic_segmentation".num_batches is changed
+                                replace = False
+                                if isinstance(param_selected, str):
+                                    if param_selected in param_name:
+                                        replace = True
+                                if isinstance(param_selected, list):
+                                    if any([p for p in param_selected if p in param_name]):
+                                        replace = True
+                                if replace:        
+                                    print(f'Replacing {param_name}')
+                                    tuned_model.model.load_state_dict({param_name: param}, strict=False)
+                        set_random_seeds(args.seed)
+                        _, _, list_metrics = tuned_model.test(df_test, IMAGE_PATH, False, False, False) 
+                        # store ade/fde 
+                        out_path = f"{out_dir_tuned}/{tuned_name}__N{'_'.join(str(n) for n in args.n_leftouts)}__{param_selected}.csv"
+                        list_metrics[0].to_csv(out_path, index=False)
+                        print(f'Saved {out_path}')
+                
+                # replace one layer in pretrained model by tuned model 
+                if args.replace_pretrained:
+                    list_metrics_tuned[0].to_csv(
+                        f"{out_dir_pretrained}/{tuned_name}__N{'_'.join(str(n) for n in args.n_leftouts)}.csv", index=False)
+                    tuned_model.load(tuned)
+                    for param_selected in params_selected:
+                        pretrained_model.load(args.pretrained)
+                        for param_name, param in tuned_model.model.named_parameters():
+                            if not param_name.startswith('semantic_segmentation'):
+                                replace = False
+                                if isinstance(param_selected, str):
+                                    if param_selected in param_name:
+                                        replace = True
+                                if isinstance(param_selected, list):
+                                    if any([p for p in param_selected if p in param_name]):
+                                        replace = True
+                                if replace:        
+                                    print(f'Replacing {param_name}')
+                                    pretrained_model.model.load_state_dict({param_name: param}, strict=False)
+                        set_random_seeds(args.seed)
+                        _, _, list_metrics = pretrained_model.test(df_test, IMAGE_PATH, False, False, False) 
+                        # store ade/fde 
+                        out_path = f"{out_dir_pretrained}/{tuned_name}__N{'_'.join(str(n) for n in args.n_leftouts)}__{param_selected}.csv"
+                        list_metrics[0].to_csv(out_path, index=False) 
+                        print(f'Saved {out_path}')
+        else:
+            raise ValueError(f'No support for depth={args.depth}')
+
+    # visualize
+    if args.replace_tuned:
+        plot_importance_analysis(
+            f'csv/importance_analysis/{folder_name}/{args.depth}/replace_tuned', 
+            f'figures/importance_analysis/{folder_name}/{args.depth}/replace_tuned',
+            n_test=args.n_leftouts[0], depth=args.depth)
+    if args.replace_pretrained:
+        plot_importance_analysis(
+            f'csv/importance_analysis/{folder_name}/{args.depth}/replace_pretrained', 
+            f'figures/importance_analysis/{folder_name}/{args.depth}/replace_pretrained',
+            n_test=args.n_leftouts[0], depth=args.depth)
 
 
 if __name__ == '__main__':
@@ -93,7 +219,10 @@ if __name__ == '__main__':
     parser.add_argument('--files', default=None, type=str, nargs='+')
     parser.add_argument('--n_leftouts', default=None, type=int, nargs='+')
     parser.add_argument('--scenes', default=None, type=str, nargs='+')
-    parser.add_argument('--depth', default=0, type=int)
+    parser.add_argument('--depth', default=-1, type=int)
+    parser.add_argument('--replace_tuned', action='store_true')
+    parser.add_argument('--replace_pretrained', action='store_true')
+    parser.add_argument('--generate_csv', action='store_true')
 
     args=parser.parse_args()
 
