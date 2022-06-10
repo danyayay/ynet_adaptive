06743241b55dc3a8e45e2d428eba2c132473654c
Danya Li <danya.li@epfl.ch>
2022-06-10 22:30:23 +0200

    fix pretrained issue

--- scripts/finetune_ped.sh
+++ scripts/finetune_ped.sh
@@ -11,14 +11,14 @@ list_train_net=(lora_1)
 list_position=("0 1 2 3 4")  # non-baseline models need position 
 list_lr=(0.0005)
 
-pretrained_ckpt=${ckpt_path}/Seed_1__Tr_Pedestrian__Val_Pedestrian__ValRatio_0.1__filter_agent_type_Biker__train__original.pt
+pretrained_ckpt=${ckpt_path}/Seed_1__Tr_Pedestrian__Val_Pedestrian__ValRatio_0.1__filter_agent_type__train__original.pt
 
 
 ########## if use sequentially split data 
-dataset_path=filter/agent_type
+dataset_path=filter/agent_type/deathCircle_0
 train_files=Biker.pkl
 val_files=Biker.pkl
-val_split=0.1
+val_split=80
 test_splits=500
 load_data=sequential
 
@@ -27,7 +27,7 @@ for train_seed in ${list_train_seed[@]}; do
         for n_train_batch in ${list_n_train_batch[@]}; do 
             for train_net in ${list_train_net[@]}; do 
                 for position in "${list_position[@]}"; do
-                    python train.py --fine_tune --seed $train_seed --batch_size $batch_size --n_epoch $n_epoch --n_round $n_round --dataset_path $dataset_path --network $network --load_data $load_data --train_files $train_files --val_files $val_files --val_split $val_split --test_splits $test_splits --train_net $train_net --position $position --ckpt_path $ckpt_path --n_train_batch $n_train_batch --lr $lr 
+                    python train.py --fine_tune --seed $train_seed --batch_size $batch_size --n_epoch $n_epoch --n_round $n_round --dataset_path $dataset_path --network $network --load_data $load_data --train_files $train_files --val_files $val_files --val_split $val_split --test_splits $test_splits --pretrained $pretrained_ckpt --train_net $train_net --position $position --ckpt_path $ckpt_path --n_train_batch $n_train_batch --lr $lr --init_check
                 done 
             done 
         done 
