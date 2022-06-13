aece6e9d70051ef724ad969f5369e6e1cb21f057
theDebugger811 <parthkothari811@gmail.com>
2022-06-13 15:13:49 +0200

    Scene 1 Car to Truck

--- inD_scripts/inD_scene1_car_truck/train_car.sh
+++ inD_scripts/inD_scene1_car_truck/train_car.sh
@@ -2,8 +2,9 @@ list_train_seed=(1)
 batch_size=10
 n_epoch=300
 n_round=3
-ckpt_path=inD_ckpt
-network=original
+ckpt_path=inD_ckpts
+network=fusion
+n_fusion=2
 train_net=train 
 
 ########## if use predefined data
@@ -12,5 +13,5 @@ load_data=predefined
 
 
 for train_seed in ${list_train_seed[@]}; do
-    python inD_train.py --config_filename 'inD_shortterm_train.yaml' --seed $train_seed --batch_size $batch_size --n_epoch $n_epoch --dataset_path $dataset_path --network $network --load_data $load_data --train_net $train_net --ckpt_path $ckpt_path --augment --n_round $n_round 
+    CUDA_VISIBLE_SCENE=0 python inD_train.py --config_filename 'inD_shortterm_train.yaml' --seed $train_seed --batch_size $batch_size --n_epoch $n_epoch --dataset_path $dataset_path --network $network --load_data $load_data --train_net $train_net  --n_fusion $n_fusion --ckpt_path $ckpt_path --augment --n_round $n_round 
 done
