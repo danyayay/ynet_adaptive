e89bb5753537db7e016b533dee0a46a90511b95a
Danya Li <danya_li@outlook.com>
2022-06-14 19:29:52 +0200

    add inD test; visualize activation

--- evaluator/visualize_activation.py
+++ evaluator/visualize_activation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from utils.parser import get_parser
 from utils.dataset import set_random_seeds, dataset_split, get_meta_ids_focus
-from utils.util_fusion import get_params, get_image_and_data_path, restore_model, get_ckpts_and_names
+from utils.util import get_params, get_image_and_data_path, restore_model, get_ckpts_and_names
 from evaluator.visualization import plot_activation, plot_activation_single
 
 
@@ -51,20 +51,20 @@ def main(args):
         # for layer_name, layer in layers_dict.items():
         #     layer.register_forward_hook(hook_store_output)
         #     layers_hook[layer_name] = layer
-        layer = model.model.encoder.stages[0][0]
-        layer.register_forward_hook(hook_store_input)
-        layers_hook['encoder.stages.0.0_input'] = layer
+        # layer = model.model.encoder.stages[0][0]
+        # layer.register_forward_hook(hook_store_input)
+        # layers_hook['encoder.stages.0.0_input'] = layer
             
         # forward 
-        pred_goal_map, pred_traj_map, raw_img, feature_input = model.forward_test(
+        pred_goal_map, pred_traj_map, raw_img = model.forward_test(
             df_test, IMAGE_PATH, set_input=[], noisy_std_frac=None) 
         
         # store 
         # for layer_name in layers_dict.keys():
         #     ckpts_hook_dict[ckpt_name][layer_name+'_output'] = layers_hook[layer_name].output
-        ckpts_hook_dict[ckpt_name]['encoder.stages.0.0_input'] = \
-            layers_hook['encoder.stages.0.0_input'].input
-        semantic_imgs = layers_hook['encoder.stages.0.0_input'].input[:, :6]
+        # ckpts_hook_dict[ckpt_name]['encoder.stages.0.0_input'] = \
+        #     layers_hook['encoder.stages.0.0_input'].input
+        # semantic_imgs = layers_hook['encoder.stages.0.0_input'].input[:, :6]
         pred_goal_map_sigmoid = model.model.sigmoid(pred_goal_map / params['temperature'])  
         pred_traj_map_sigmoid = model.model.sigmoid(pred_traj_map / params['temperature'])
         ckpts_hook_dict[ckpt_name]['goal_decoder.predictor_sigmoid'] = pred_goal_map_sigmoid
@@ -74,7 +74,7 @@ def main(args):
         
         for key, value in ckpts_hook_dict[ckpt_name].items():
             print(key, value.shape)
-        print('semantic_imgs', semantic_imgs.shape)
+        # print('semantic_imgs', semantic_imgs.shape)
 
     # plot 
     folder_name = f"{args.seed}__{'_'.join(args.dataset_path.split('/'))}__{'_'.join(args.val_files).rstrip('.pkl')}/{'_'.join(ckpts_name)}" 
@@ -87,15 +87,11 @@ def main(args):
     plot_activation_single(
         ckpts_hook_dict, index, df_test, IMAGE_PATH, 
         out_dir='figures/activation_yy', display_scene_img=False,
-        inhance_threshold=0.15, format='png')
-    plot_activation_single(
-        ckpts_hook_dict, index, df_test, IMAGE_PATH, 
-        out_dir='figures/activation_yy', display_scene_img=False,
-        inhance_threshold=0.1, format='pdf')
-    plot_activation_single(
-        ckpts_hook_dict, index, df_test, IMAGE_PATH, 
-        out_dir='figures/activation_yy', display_scene_img=False,
-        inhance_threshold=0.15, format='pdf')
+        inhance_threshold=0.05, format='png')
+    # plot_activation_single(
+    #     ckpts_hook_dict, index, df_test, IMAGE_PATH, 
+    #     out_dir='figures/activation_yy', display_scene_img=False,
+    #     inhance_threshold=0.15, format='png')
     # plot_activation_single(
     #     ckpts_hook_dict, index, df_test, IMAGE_PATH, 
     #     out_dir='figures/activation_yy', display_scene_img=True,
@@ -140,10 +136,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
     main(args)
 
-# python -m pdb -m evaluator.visualize_activation --dataset_path filter/agent_type/deathCircle_0 --pretrained_ckpt ckpts/Seed_1__Tr_Pedestrian__Val_Pedestrian__ValRatio_0.1__filter_agent_type__train.pt --tuned_ckpts ckpts/DC0__lora/Seed_1__Tr_Biker__Val_Biker__ValRatio_0.1__filter_agent_type_deathCircle_0__lora_1__Pos_0_1_2_3_4__TrN_20__lr_0.0005.pt --val_files Biker.pkl --n_leftouts 500 
+# python -m pdb -m evaluator.visualize_activation --dataset_path filter/agent_type/deathCircle_0 --pretrained_ckpt ckpts/Seed_1__Tr_Pedestrian__Val_Pedestrian__ValRatio_0.1__filter_agent_type__train__original --tuned_ckpts ckpts/lora/rank1/Seed_1__Tr_Biker__Val_Biker__ValRatio_0.1__filter_agent_type_deathCircle_0__lora_1__Pos_0_1_2_3_4__TrN_20__lr_0.0005.pt --val_files Biker.pkl --n_leftouts 500 --given_meta_ids 5358 5883 5982
 
 # --given_meta_ids 5358 5883 5982
 
 # --given_meta_ids 5711 6060 5670 5334 6063 6269 5885 6310 5767 6322 5445 5358 5726 6230 5468 5890 5883 5715 5450 6228 
 
-# --result_path "csv/comparison/1__filter_agent_type_deathCircle_0__Biker/OODG_lora_1[0_1_2_3_4]20__N500.csv" --result_name "ade_lora_1[0_1_2_3_4](20)__fde_lora_1[0_1_2_3_4](20)__diff" --result_limited 100
